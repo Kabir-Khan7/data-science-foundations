2472279a02d1d623d9046d380e1776d1076a2729
Kabir-Khan7 <kabirkhurshid.official@gmail.com>
2025-12-23 02:53:30 +0500

    Step_04

--- 04_Step_Data_Visualization/.ipynb_checkpoints/main-checkpoint.py
+++ 04_Step_Data_Visualization/.ipynb_checkpoints/main-checkpoint.py
@@ -1,8 +1,7 @@
 import matplotlib.pyplot as plt
-# plt.plot([1,2,3], [4,5,6])
-# plt.show()
+plt.style.use("ggplot")
 
-
+#First Example
 years = [1990, 1992, 1994, 1996, 1998, 2000, 2003, 2005, 2007, 2010]
 runs =  [500, 700, 1100, 1500, 1800, 1200, 1700, 1300, 900, 1500]
 plt.plot(years, runs)
@@ -10,5 +9,17 @@ plt.plot(years, runs)
 plt.xlabel("Year")
 plt.ylabel("Run Scored")
 plt.title("Sachin Tendulkar's Yearly Runs")
-
 plt.show()
+
+
+#Second Example
+labels = ["Sachin", "Sehwag", "Kohli", "Yuvraj"]
+runs = [18000,8000,12000,9500]
+colors = ["#C93616", "#57D10F", "#17E8D7", "#1046C2"]
+
+
+plt.title("Career Runs of Indian Batsman")
+plt.pie(runs, labels=labels, colors=colors)
+plt.show()
+
+

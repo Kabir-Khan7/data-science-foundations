23d4287995c0aaf772dcb994e06057738488233d
Kabir-Khan7 <kabirkhurshid.official@gmail.com>
2026-01-02 22:12:44 +0500

    Step_04

--- 04_Step_Data_Visualization/main.py
+++ 04_Step_Data_Visualization/main.py
@@ -1,25 +1,29 @@
-import matplotlib.pyplot as plt
-plt.style.use("ggplot")
+# import matplotlib.pyplot as plt
+# plt.style.use("ggplot")
 
-#First Example
-years = [1990, 1992, 1994, 1996, 1998, 2000, 2003, 2005, 2007, 2010]
-runs =  [500, 700, 1100, 1500, 1800, 1200, 1700, 1300, 900, 1500]
-plt.plot(years, runs)
-plt.plot(years, runs)
-plt.xlabel("Year")
-plt.ylabel("Run Scored")
-plt.title("Sachin Tendulkar's Yearly Runs")
-plt.show()
-
-
-#Second Example
-labels = ["Sachin", "Sehwag", "Kohli", "Yuvraj"]
-runs = [18000,8000,12000,9500]
-colors = ["#C93616", "#57D10F", "#17E8D7", "#1046C2"]
+# #First Example
+# years = [1990, 1992, 1994, 1996, 1998, 2000, 2003, 2005, 2007, 2010]
+# runs =  [500, 700, 1100, 1500, 1800, 1200, 1700, 1300, 900, 1500]
+# plt.plot(years, runs)
+# plt.plot(years, runs)
+# plt.xlabel("Year")
+# plt.ylabel("Run Scored")
+# plt.title("Sachin Tendulkar's Yearly Runs")
+# plt.show()
 
 
-plt.title("Career Runs of Indian Batsman")
-plt.pie(runs, labels=labels, colors=colors)
-plt.show()
+# #Second Example
+# labels = ["Sachin", "Sehwag", "Kohli", "Yuvraj"]
+# runs = [18000,8000,12000,9500]
+# colors = ["#C93616", "#57D10F", "#17E8D7", "#1046C2"]
 
 
+# plt.title("Career Runs of Indian Batsman")
+# plt.pie(runs, labels=labels, colors=colors)
+# plt.show()
+
+
+f = open("eng1.csv", "r")
+f.read()
+
+print("Hello World")
